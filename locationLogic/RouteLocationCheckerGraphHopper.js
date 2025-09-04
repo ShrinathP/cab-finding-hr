a9c43fbc494488377feffb99771a5ffaf2834ae1
Shrinath <shrinath.potul@nutanix.com>
2025-09-04 12:04:39 +0530

    changes to the routeLocationGraphhopper to use public api

--- locationLogic/RouteLocationCheckerGraphHopper.js
+++ locationLogic/RouteLocationCheckerGraphHopper.js
@@ -5,25 +5,61 @@ class RouteLocationChecker {
   constructor() {
     this.graphhopperUrl = "http://localhost:8989/route";  // URL for GraphHopper
   }
+  
+  // Helper function to GET ROUTING PATH FROM GET GRAPHHOPPER API USING API KEY
+  async fetchRouteFromGraphhopperGetRouteApi(pickup, destination) {
+    const API_KEY = process.env.GRAPHHOPPER_API_KEY;
+    const url = `https://graphhopper.com/api/1/route?point=${pickup.lat},${pickup.lng}&point=${destination.lat},${destination.lng}&profile=car&instructions=true&type=json&key=${API_KEY}`;
+    const response = await axios.get(url);
+    console.log(response);
+    return response;
+  }
+
+  async fetchRouteFromGraphhopperPostRouteApi(pickup, destination) {
+    const API_KEY = process.env.GRAPHHOPPER_API_KEY;
+    const url = `https://graphhopper.com/api/1/route?key=${API_KEY}`;
+    // Define the data to be sent in the POST request
+    const data = {
+      profile: "car", // Change this to "bike" if needed
+      points: [
+        [pickup.lat, pickup.lng], // Pickup point
+        [destination.lat, destination.lng]   // Destination point
+      ],
+      point_hints: [
+        "Lindenschmitstraße", // Optional hints for the points
+        "Thalkirchener Str."
+      ],
+      snap_preventions: [
+        "motorway", // Areas to avoid
+        "ferry",
+        "tunnel"
+      ],
+      details: [
+        "road_class", // Extra details you want to include
+        "surface"
+      ]
+    };
+
+    // Make the POST request
+    axios.post(url, data, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
 
   // Fetch route polyline from GraphHopper
   async getRoutePolyline(pickup, destination) {
     try {
-      // const response = await axios.get(this.graphhopperUrl, {
-      //   params: {
-      //     point: `${pickup.lat},${pickup.lng}`,
-      //     point: `${destination.lat},${destination.lng}`,
-      //     vehicle: "car",
-      //     instructions: true,
-      //     type: "json",
-      //   },
-      // });
-
-      // const url = `http://127.0.0.1:8989/route?point=${pickup.lng},${pickup.lat}&point=${destination.lng},${destination.lat}&profile=car&instructions=true&type=json`
+      // GET ROUTING PATH FROM DOCKER CONTAINER OF GRAPHHOPPER
+      /*
       const url = `http://127.0.0.1:8989/route?point=${pickup.lat},${pickup.lng}&point=${destination.lat},${destination.lng}&profile=car&instructions=true&type=json`
       const response = await axios.get(url);
       console.log(response);
-      
+      */
+
+      // GET ROUTING PATH FROM GET GRAPHHOPPER API USING API KEY
+      const response = await this.fetchRouteFromGraphhopper(pickup, destination);
 
       if (response.data.paths.length === 0) {
         throw new Error("No routes found");
